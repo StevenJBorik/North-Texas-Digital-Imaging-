28acdc5c06a3d0d95fb02895de187bdd5daefb0a
Steven Borik <98104308+StevenJBorik@users.noreply.github.com>
2023-04-03 13:15:42 -0500

    hamburger menu fix

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -4,6 +4,14 @@
 const navbarMenu = document.querySelector("#navigation #navbar-menu");
 const hamburgerMenu = document.querySelector("#navigation .hamburger-menu");
 
+// const hamburgerBtn = document.querySelector('#navigation .hamburger-menu');
+// const navbarMenu = document.querySelector('#navbar-menu');
+
+// Get the current URL path
+var url = window.location.pathname;
+console.log(url); 
+
+
 hamburgerMenu.addEventListener('click', function() {
     const isNavOpen = navbarMenu.classList.contains("open");
     if (!isNavOpen) {
@@ -11,9 +19,12 @@ hamburgerMenu.addEventListener('click', function() {
         hamburgerMenu.setAttribute("aria-expanded", true);
         hamburgerMenu.classList.add("clicked");
         navbarMenu.classList.add("open");
+
     } else {
+        console.log("ugh");
         hamburgerMenu.setAttribute("aria-expanded", false);
         hamburgerMenu.classList.remove("clicked");
         navbarMenu.classList.remove("open");
     }
 });
+
