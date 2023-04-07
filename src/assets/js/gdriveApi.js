9ea9e9d5fc1156deb4ef984f27f9a675f635aa76
Steven Borik <98104308+StevenJBorik@users.noreply.github.com>
2023-04-07 01:32:09 -0500

    updated gapi.auth2 to google auth library

--- src/assets/js/gdriveApi.js
+++ src/assets/js/gdriveApi.js
@@ -1,46 +1,58 @@
-let gapiLoaded = false;
+const { GoogleAuth } = require('google-auth-library')
 
-function initGoogleDrive() {
-  if (!gapiLoaded) {
-    gapiLoaded = true;
-    const script = document.createElement("script");
-    script.src = "https://apis.google.com/js/api.js";
-    script.defer = true;
-    script.async = true;
+let gdriveApiLoaded = false;
+const CLIENT_ID = '830042434681-96p23o3fm7to4vejl103d2hr5oi2s38f.apps.googleusercontent.com';
+const REDIRECT_URI = 'http://localhost:8080/mriForm';
+const FILE_ID = '1tkW2QEB_CVLz0iJqR3iKn6BKnY6AHfUO2VuDkvZegHk';
+const IFRAME_ID = 'pdf-iframe';
+const PARENT_FOLDER_ID = '10XCaaS_hQrXJR21frN9kmqTVIfxYtOLf';
+
+
+function loadGDriveApi() {
+  console.log('Loading GDrive API...');
+
+  return new Promise((resolve, reject) => {
+    // Load the Google API client library asynchronously
+    const script = document.createElement('script');
+    script.src = 'https://apis.google.com/js/api.js';
     document.body.appendChild(script);
+
+    // Listen for the API library to load
     script.onload = () => {
-      gapi.load("client:auth2", initClient);
+      console.log('Google API client library loaded.');
+
+      // Initialize the Google API client with your OAuth 2.0 client ID and API key
+      gapi.load('client:auth2', () => {
+        gapi.client.init({
+          clientId: CLIENT_ID,
+          discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'],
+          scope: 'https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/drive.install',
+          redirectUri: REDIRECT_URI
+        }).then(() => {
+          console.log('Google API client initialized.');
+          gdriveApiLoaded = true;
+          resolve();
+        }).catch((error) => {
+          console.error('Error initializing Google API client:', error);
+          reject(error);
+        });
+      });
     };
-    
-  } else {
-    initClient();
-  }
+  });
 }
 
-function initClient() {
-  const isAuthorizedOrigin = window.location.origin === 'http://localhost:8080';
-  const isAuthorizedRedirectUri = window.location.pathname === 'http://localhost:8080/mriForm';
+async function displayFileInIframe(fileId) {
+  console.log('Inside displayFileInIframe function');
+  const iframe = document.getElementById(IFRAME_ID);
 
-  if (!isAuthorizedOrigin || !isAuthorizedRedirectUri) {
-    console.log('Unauthorized origin or redirect URI');
+  if (!iframe) {
+    console.log('Iframe not found');
     return;
   }
 
-  return gapi.client.init({
-    apiKey: "AIzaSyBU9xNLl6-KMOtcOrjQ0rf6B9uxnN5w31A",
-    clientId: "830042434681-96p23o3fm7to4vejl103d2hr5oi2s38f.apps.googleusercontent.com",
-    scope: "https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/drive.install",
-    discoveryDocs: ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"],
-    cookie_policy: "single_host_origin"
-  }).then(() => loadDriveApi())
-    .then(() => getFile())
-    .then(() => copyFile())
-    .then((response) => {
-      const webViewLink = response.webViewLink;
-      console.log(webViewLink);
-      // generate iframe with webViewLink as src
-    })
-    .catch((err) => console.log(err));
+  const iframeSrc = await getIframeContentUrl(fileId);
+
+  iframe.src = iframeSrc;
 }
 
 function getIframeContentUrl(fileId) {
@@ -62,90 +74,65 @@ function getIframeContentUrl(fileId) {
   });
 }
 
-async function displayFileInIframe(fileId) {
-  console.log('inside displayFileInIframe function');
-  const iframe = document.getElementById('pdf-iframe');
+async function copyAndDisplayFile() {
+  console.log('Inside copyAndDisplayFile function');
 
-  if (!iframe) {
-    console.log('Iframe not found');
+  if (!gdriveApiLoaded) {
+    console.error('GDrive API not loaded.');
     return;
   }
 
-  const iframeSrc = await getIframeContentUrl(fileId);
-
-  iframe.src = iframeSrc;
-}
-
-async function copyFileAndDisplayInIframe() {
   try {
-    const response = await copyFile();
-    const fileId = response.id;
-    await displayFileInIframe(fileId);
+    // Create a new instance of GoogleAuth
+    const auth = await new google.auth.GoogleAuth({
+      clientId: CLIENT_ID,
+      scopes: ['https://www.googleapis.com/auth/drive.file'] // or no file
+    });
+
+    // Get authorization from the user
+    const {tokens} = await auth.getClient();
+
+    // Set the access token for the Google API client
+    gapi.auth.setToken(tokens);
+
+    // Copy the file
+    const response = await gapi.client.drive.files.copy({
+      fileId: FILE_ID,
+      resource: {
+        'name': 'Copy of MRI Patient Form',
+        'parents': [PARENT_FOLDER_ID]
+      }
+    });
+    console.log('File copied successfully:', response);
+
+    // Update the src attribute of the iframe with the URL of the copied file
+    const pdfIframe = document.getElementById(IFRAME_ID);
+    pdfIframe.src = await getIframeContentUrl(response.result.id);
   } catch (error) {
-    console.error('Error copying file and displaying in iframe:', error);
+    console.error('Error copying or displaying file:', error);
   }
 }
 
 
-function loadDriveApi() {
-  return gapi.client.load("drive", "v3");
-}
+function init() {
+  console.log('Initializing...');
 
-function getFile() {
-  console.log('inside getFile Function');
+  // Load the Google Drive API
+  loadGDriveApi().then(() => {
+    console.log('Google Drive API loaded.');
 
-  const fileId = '1tkW2QEB_CVLz0iJqR3iKn6BKnY6AHfUO2VuDkvZegHk';
+    // Add event listener to the copy button
+    const copyButton = document.getElementById('auth-button');
+    copyButton.addEventListener('click', copyAndDisplayFile);
 
-  return new Promise((resolve, reject) => {
-    const request = gapi.client.drive.files.get({
-      fileId,
-      alt: 'media'
+    // Add event listener to the iframe to automatically resize its height
+    const iframe = document.getElementById('pdf-iframe');
+    iframe.addEventListener('load', () => {
+      iframe.style.height = `${iframe.contentWindow.document.body.scrollHeight}px`;
     });
-
-    request.execute((response) => {
-      if (response.error) {
-        console.error('Error getting file:', response.error);
-        reject(response.error);
-        return;
-      }
-      console.log('File fetched successfully:', response);
-      resolve(response);
-    });
+  }).catch((error) => {
+    console.error('Error loading Google Drive API:', error);
   });
 }
 
-function copyFile() {
-  console.log('inside copyFile Function');
-
-  const fileId = '1tkW2QEB_CVLz0iJqR3iKn6BKnY6AHfUO2VuDkvZegHk';
-  const copyTitle = 'Copy of MRI Patient Form';
-  const copyMetadata = {
-      'name': copyTitle,
-      'parents': ['10XCaaS_hQrXJR21frN9kmqTVIfxYtOLf']
-  };
-
-  return new Promise((resolve, reject) => {
-      const request = gapi.client.drive.files.copy({
-          fileId,
-          resource: copyMetadata
-      });
-      request.execute((response) => {
-          if (response.error) {
-              console.error('Error copying file:', response.error);
-              reject(response.error);
-              return;
-          }
-          console.log('File copied successfully:', response);
-
-          // Update the src attribute of the iframe with the URL of the copied file
-          const pdfIframe = document.getElementById('pdf-iframe');
-          pdfIframe.src = response.webContentLink;
-
-          resolve(response);
-      });
-  });
-}
-
-initGoogleDrive();
-copyFileAndDisplayInIframe();
-
+init();
